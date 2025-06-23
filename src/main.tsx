925997ba77df030cc38fdc56cb3bc680cd2551e0
uty <fgmapam@gmail.com>
2025-06-23 21:04:31 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@
 import 'modern-normalize/modern-normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
