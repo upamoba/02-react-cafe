b400b28684b8d66f0a9b886b9cdf9a154aa56b9f
uty <fgmapam@gmail.com>
2025-06-23 21:09:51 +0300

    summary(required)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import css from './App.module.css';
 import 'modern-normalize/modern-normalize.css';
 
-import CafeInfo from './components/CafeInfo/CafeInfo';
+import CafeInfo from './components/ggg/CafeInfo';
 import VoteOptions from './components/VoteOptions/VoteOptions';
 import VoteStats from './components/VoteStats/VoteStats';
 import Notification from './components/Notification/Notification';
