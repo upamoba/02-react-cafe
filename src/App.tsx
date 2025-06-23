17cfdee48ff4c2eba317f2cd24d42f46e13cc645
uty <fgmapam@gmail.com>
2025-06-23 21:04:01 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,9 @@ import type { Votes, VoteType } from './types/votes';
 const App: React.FC = () => {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
-  const handleVote = (type: VoteType) =>
-    setVotes((prev: Votes) => ({ ...prev, [type]: prev[type] + 1 }));
+  const handleVote = (type: VoteType) =>{
+    setVotes((prev) => ({ ...prev, [type]: prev[type] + 1 }));
+  };
 
   const resetVotes = () => setVotes({ good: 0, neutral: 0, bad: 0 });
 
