73790e9cf34ca1a71e83f21f7fa0ef9163c923d8
uty <fgmapam@gmail.com>
2025-06-23 20:57:09 +0300

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
   const handleVote = (type: VoteType) =>
-    setVotes(prev => ({ ...prev, [type]: prev[type] + 1 }));
+    setVotes((prev: Votes) => ({ ...prev, [type]: prev[type] + 1 }));
 
   const resetVotes = () => setVotes({ good: 0, neutral: 0, bad: 0 });
 
